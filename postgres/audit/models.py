ab30540b5626d07718cdc3a198b26291caa728bd
Matthew Schinckel <matt@schinckel.net>
2014-06-29 23:50:49 +0930

    Improved AuditLog model.
    Now possible to get nice represention of fields, and changed data.
    
    Possible to get old/new instances of objects. Refactored how these
    are generated, so we can get the model class (and the verbose name)
    along the way, which is useful for template rendering.

--- postgres/audit/models.py
+++ postgres/audit/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models, connection
+from django.utils.functional import cached_property
 
 import postgres.fields.json_field
 import postgres.fields.internal_types
@@ -11,7 +12,7 @@ def get_model_from_table(table):
     if not known_models:
         for model in models.get_models():
             known_models[model._meta.db_table] = model
-    
+
     return known_models[table]
 
 
@@ -24,29 +25,27 @@ class AuditLogQuerySet(models.QuerySet):
             table_name=instance._meta.db_table,
             **pk
         )
-    
+
     def inserts(self):
         return self.filter(action='I')
-    
+
     def updates(self):
         return self.filter(action='U')
-    
+
     def deletes(self):
         return self.filter(action='D')
-    
+
     def after(self, when):
         return self.filter(timestamp__gt=when)
-    
+
     def before(self, when):
         return self.filter(timestamp__lt=when)
-    
+
     def between(self, start, finish):
         return self.before(finish).after(start)
-    
+
     def by_user(self, user):
         return self.filter(app_user=user.pk)
-    
-
 
 
 class AuditLog(models.Model):
@@ -55,7 +54,7 @@ class AuditLog(models.Model):
     we shouldn't use it for saving. Perhaps we could have the
     underlying system create a read-only view so any saves fail.
     """
-    
+
     action = models.TextField(choices=[
         ('I', 'INSERT'),
         ('U', 'UPDATE'),
@@ -72,9 +71,9 @@ class AuditLog(models.Model):
     changed_fields = postgres.fields.json_field.JSONField(null=True)
     app_user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
     app_ip_address = models.IPAddressField(null=True)
-    
+
     objects = AuditLogQuerySet.as_manager()
-    
+
     @property
     def current_instance(self):
         model = get_model_from_table(self.table_name)
@@ -85,17 +84,25 @@ class AuditLog(models.Model):
             pk.name, self.row_data[pk.name]
         )
         return model._default_manager.raw(query)[0]
-    
-    @property
-    def old_instance(self):
-        # Generate a copy of what the instance looked like before this
-        # event (in the case of a delete or update), or after the event
-        # in the case of a create.
-        model = get_model_from_table(self.table_name)
-        data = dict(self.row_data)
-        if self.action in ['D', 'U']:
-            data.update(**self.changed_fields)
-            # Remap db_column to django field name.
+
+    @cached_property
+    def model_class(self):
+        return get_model_from_table(self.table_name)
+
+    @cached_property
+    def model_name(self):
+        return self.model_class._meta.verbose_name
+
+    def _build_model(self, data):
+        """
+        Given a dict of data, convert it into a dict with
+        the correct field names, and related objects.
+        """
+        model = self.model_class
+        # Make a copy, as we may mutate it.
+        data = dict(data)
+
+        # Remap db_column to django field name.
         fields = []
         for field in model._meta.fields:
             fields.append(field.name)
@@ -105,7 +112,70 @@ class AuditLog(models.Model):
                 data[field.name] = field.rel.to.objects.get(pk=data.pop(db_column))
             elif field.db_column:
                 data[field.name] = data.pop(field.db_column)
-        
+
         data = dict((key,value) for (key,value) in data.items() if key in fields)
-        
+
         return model(**data)
+
+    @cached_property
+    def field_name_mapping(self):
+        fields = {}
+        model = self.model_class
+        for field in model._meta.fields:
+            fields[field.db_column or field.name] = field.name
+        return fields
+
+    @cached_property
+    def field_title_mapping(self):
+        fields = {}
+        model = self.model_class
+        for field in model._meta.fields:
+            if field.primary_key or not field.editable:
+                continue
+            fields[field.db_column or field.name] = field.verbose_name
+        return fields
+
+    @cached_property
+    def changed_data(self):
+        field_names = self.field_name_mapping
+        field_titles = self.field_title_mapping
+        new_instance = self.new_instance
+
+        if self.action == 'I':
+            # New object! all fields are new.
+            return [{
+                'field': field_titles[key],
+                'new': getattr(new_instance, field_names[key])
+            } for key in self.row_data if key in field_titles
+            ]
+
+        old_instance = self.old_instance
+
+        return [
+            {
+                'field': field_titles[key],
+                'old': getattr(old_instance, field_names[key], ''),
+                'new': getattr(new_instance, field_names[key], ''),
+            } for key in self.changed_fields
+            if key in field_titles
+        ]
+
+    @cached_property
+    def old_instance(self):
+        """
+        What the instance looked like before the change.
+
+        For an INSERT, it will be None: for UPDATE/DELETE, it
+        will be the data that was stored.
+        """
+        if self.action == 'I':
+            return None
+        return self._build_model(self.row_data)
+
+    @property
+    def new_instance(self):
+        data = dict(self.row_data)
+        if self.action == 'U':
+            data.update(**self.changed_fields)
+        return self._build_model(data)
+
