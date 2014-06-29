00a0971f9bf3b91b0a88aeb2b9a37cea095fc8fd
Matthew Schinckel <matt@schinckel.net>
2014-06-29 23:47:41 +0930

    Add ability for DateField to search on DateRange.

--- postgres/fields/range_fields.py
+++ postgres/fields/range_fields.py
@@ -2,7 +2,7 @@ from django.db import models
 from django import forms
 from django.utils import six
 
-from psycopg2._range import Range
+from psycopg2._range import Range, DateRange
 
 # Monkey patch Range so that we get a a string that can
 # be used to save.
@@ -15,6 +15,7 @@ def range_to_string(value):
     return value
 
 Range.__unicode__ = range_to_string
+
 
 class RangeField(models.Field):
     pass
@@ -40,5 +41,31 @@ class RangeOverlapsLookup(models.Lookup):
         params = lhs_params + rhs_params
         return '%s && %s' % (lhs, rhs), params
 
-
 RangeField.register_lookup(RangeOverlapsLookup)
+
+
+def InRangeFactory(RangeType):
+    class InRange(models.Lookup):
+        lookup_name = 'in'
+        range_cast = RangeType.__name__.lower()
+        column_cast = range_cast.replace('range', '')
+
+        def __init__(self, lhs, rhs):
+            self.lhs, self.rhs = lhs, rhs
+            if not isinstance(self.rhs, RangeType):
+                self.rhs = self.get_prep_lookup()
+
+        def as_sql(self, qn, connection):
+            if not isinstance(self.rhs, RangeType):
+                return super(InRange, self).as_sql(qn, connection)
+
+            lhs, lhs_params = self.process_lhs(qn, connection)
+            rhs, rhs_params = '%s', [unicode(self.rhs)]
+            params = lhs_params + rhs_params
+            return '%s::%s <@ %s::%s' % (
+                lhs, self.column_cast, rhs, self.range_cast
+            ), params
+
+    return InRange
+
+models.DateField.register_lookup(InRangeFactory(DateRange))
