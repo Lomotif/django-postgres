57209897d3d5646896590cb35852c6a717e8a748
Matthew Schinckel <matt@schinckel.net>
2014-07-02 01:25:11 +0930

    Make standalone search results page.

--- postgres/search/views.py
+++ postgres/search/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.utils.html import conditional_escape as escape
 
+from ..views import FormListView, AjaxTemplateMixin
 from .models import Search
+from .forms import SearchForm
 
 def uikit(request):
     searches = Search.objects.matches(request.GET['search'])
@@ -18,3 +20,12 @@ def uikit(request):
             } for search in searches
         ]
     }))
+
+
+
+class SearchResults(AjaxTemplateMixin, FormListView):
+    form_class = SearchForm
+    context_object_name = 'search_results'
+    paginate_by = 25
+
+results = SearchResults.as_view(template_name='search/page.html', ajax_template_name='search/form.html')
