f4a7e1198c5a8b0e7dd60b6645e1ad46d9d9a340
Matthew Schinckel <matt@schinckel.net>
2014-11-18 18:19:02 +1030

    Add better tstz_range support

---
+++ postgres/fields/interval_field.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class IntervalField(models.Field):
+    description = 'Interval Field'
+
+    def db_type(self, connection):
+        return 'interval'
+
+    def get_internal_type(self):
+        return 'IntervalField'
+
+    def get_prep_value(self, value):
+        if value == '' and self.null:
+            return None
+        return value
