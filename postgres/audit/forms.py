83c0dd06aa5d176d2d0c75cef487da22478b931c
Matthew Schinckel <matt@schinckel.net>
2015-10-05 09:06:07 +1030

    Remove Python 3.2 tests

---
+++ postgres/audit/forms.py
@@ -0,0 +1,17 @@
+import datetime
+
+from django import forms
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+
+from .models import AuditLog
+
+
+class AuditQueryForm(forms.Form):
+    start = forms.DateField()
+    finish = forms.DateField()
+
+    def get_queryset(self):
+        start = self.cleaned_data['start']
+        finish = self.cleaned_data['finish'] + datetime.timedelta(1)
+
+        return AuditLog.objects.filter(timestamp__range=(start, finish)).order_by('-timestamp')
---
+++ postgres/fields/__init__.py
@@ -0,0 +1,6 @@
+from .composite_field import *  # NOQA
+from .json_field import JSONField
+from .interval_field import IntervalField
+from .range_fields import Int4RangeField
+from .ts_fields import TSVectorField
+from .uuid_field import UUIDField
